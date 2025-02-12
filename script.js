92d1774050a40c9830ec7f978b050f6f53805b0d
SB Designs <hello@sbdesigns.spike.team>
2025-02-12 19:53:55 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('animatedButton').addEventListener('click', function() {
+    // Redirect to Age Concern Southend's website
+    window.location.href = 'https://www.ageconcernsouthend.co.uk';  // Replace this with the actual URL
+});
